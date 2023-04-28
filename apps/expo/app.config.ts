a1deb68edfb1e94f3797df77eb21d1d674923441
Aaron Motacek <aaronmotacek@gmail.com>
2023-04-28 15:48:01 -0500

    initial setup complete

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_cmVsYXRlZC1nb2F0LTMzLmNsZXJrLmFjY291bnRzLmRldiQ";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
